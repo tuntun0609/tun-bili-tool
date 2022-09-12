681a104374c5055f21345c2d6bff5ceeed9db646
tuntun <56946732+tuntun0609@users.noreply.github.com>
2022-09-13 02:32:48 +0800

    fix: 修复因为antd中Menu组件导致options页面报错白屏的错误

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import { Menu } from 'antd';
-import type { MenuProps } from 'antd';
+import React, { ReactNode, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import './SideBar.scss';
 
-export const routerItems: MenuProps['items'] = [
+export const routerItems: { label: ReactNode, key: string }[] = [
 	{ label: '主站设置', key: 'main-setting' },
 	{ label: '视频设置', key: 'video-setting' },
 	{ label: '直播设置', key: 'live-setting' },
@@ -14,18 +12,26 @@ export const routerItems: MenuProps['items'] = [
 
 export const SideBar: React.FC = () => {
 	const navigate = useNavigate();
+	const [selectItem, setSelectItem] = useState(0);
 	const onMenuClick = (e: any) => {
 		navigate(e.key);
 	};
 	return (
-		<div className='side-bar'>
-			<Menu
-				style={{ width: 258, borderRight: 'none' }}
-				defaultSelectedKeys={['1']}
-				defaultOpenKeys={['sub1']}
-				items={routerItems}
-				onClick={onMenuClick}
-			/>
+		<div className='sidebar'>
+			{
+				routerItems.map((item, index) => (
+					<div
+						key={item.key}
+						className={'sidebar-item' + (selectItem === index ? ' sidebar-item-select' : '')}
+						onClick={() => {
+							onMenuClick(item);
+							setSelectItem(index);
+						}}
+					>
+						<div className={'sidebar-item-label' + (selectItem === index ? ' sidebar-item-label-select' : '')}>{item.label}</div>
+					</div>
+				))
+			}
 		</div>
 	);
 };
