ed19c36195fce0946b9cba37a5f1e75a9f9257fe
tuntun <56946732+tuntun0609@users.noreply.github.com>
2022-10-07 02:36:32 +0800

    fix: 修复未登录情况下popup视频动态页面报错

---
+++ src/components/NotLogin/NotLogin.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const NotLogin: React.FC = () => (
+	<div
+		style={{
+			textAlign: 'center',
+			padding: '10px',
+		}}
+	>未登录，请登录体验完整功能</div>
+);
--- src/pages/PopupVideoList/PopupDynHome.tsx
+++ src/pages/PopupVideoList/PopupDynHome.tsx
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import { PopupDynVideoItem, InfiniteScrollLoader } from '../../components';
+import { PopupDynVideoItem, InfiniteScrollLoader, NotLogin } from '../../components';
 import { API } from '../../utils';
 
 export const PopupDynHome: React.FC = () => {
 	const [videoList, setVideoList] = useState<any[]>([]);
+	const [login, setLogin] = useState(true);
 	const [page, setPage] = useState(1);
 	const [offset, setOffset] = useState<string>(undefined);
 	const [loading, setLoading] = useState(false);
 	const getVideoList = async () => {
 		try {
 			const data = await API.getDynVideo(page, offset);
+			if (data.code === -101 && data.message === '账号未登录') {
+				setLogin(false);
+				return;
+			}
 			setPage(page + 1);
-			setVideoList(videoList.concat(data?.data?.items));
+			setVideoList(videoList.concat(data?.data?.items ?? []));
 			setOffset(data?.data?.offset);
 		} catch (error) {
 			console.error(error);
@@ -35,29 +40,31 @@ export const PopupDynHome: React.FC = () => {
 	}, []);
 	return (
 		<div className='popup-list' id='dynVideoScrollableDiv'>
-			<InfiniteScroll
-				dataLength={videoList.length}
-				next={loadMoreData}
-				hasMore={true}
-				loader={<InfiniteScrollLoader />}
-				scrollableTarget="dynVideoScrollableDiv"
-				style={{
-					display: 'flex',
-					justifyContent: 'space-between',
-					flexWrap: 'wrap',
-					alignContent: 'flexStart',
-					overflow: 'visible',
-				}}
-			>
-				{
-					videoList.map(item => (
-						<PopupDynVideoItem
-							key={item.id_str}
-							data={item}
-						></PopupDynVideoItem>
-					))
-				}
-			</InfiniteScroll>
+			{login
+				? <InfiniteScroll
+					dataLength={videoList.length}
+					next={loadMoreData}
+					hasMore={true}
+					loader={<InfiniteScrollLoader />}
+					scrollableTarget="dynVideoScrollableDiv"
+					style={{
+						display: 'flex',
+						justifyContent: 'space-between',
+						flexWrap: 'wrap',
+						alignContent: 'flexStart',
+						overflow: 'visible',
+					}}
+				>
+					{
+						videoList.map(item => (
+							<PopupDynVideoItem
+								key={item.id_str}
+								data={item}
+							></PopupDynVideoItem>
+						))
+					}
+				</InfiniteScroll>
+				: <NotLogin />}
 		</div>
 	);
 };
