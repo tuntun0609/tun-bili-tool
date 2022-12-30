9f0130d27e8ee18055e710c3908b86c5132225d6
tuntun <760523867@qq.com>
2022-12-31 05:45:58 +0800

    fix(showsamefollowings): 重新渲染导致样式丢失问题

--- src/utils/getMessageConfig.ts
+++ src/utils/getMessageConfig.ts
@@ -1,8 +1,9 @@
 import type { ConfigOptions } from 'antd/es/message/interface';
 
-export const getMessageConfig = (selector: string): ConfigOptions => ({
+export const getMessageConfig = (selector?: string, config?: ConfigOptions): ConfigOptions => ({
 	top: 70,
 	duration: 1.5,
 	maxCount: 3,
-	getContainer: () => document.querySelector(selector).shadowRoot.querySelector('#plasmo-shadow-container'),
+	getContainer: () => selector ? document.querySelector(selector).shadowRoot.querySelector('#plasmo-shadow-container') : document.body,
+	...config,
 });
