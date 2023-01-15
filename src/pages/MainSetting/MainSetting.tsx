721501b0331fc69fc8f86ab3dba312cbc4c6e9b3
tuntun <760523867@qq.com>
2023-01-15 16:15:59 +0800

    refactor(options): 重构主站设置表单

--- src/pages/MainSetting/MainSetting.tsx
+++ src/pages/MainSetting/MainSetting.tsx
@@ -1,72 +1,84 @@
 import React from 'react';
 import { useStorage } from '@plasmohq/storage/hook';
-
-import { FromType, SettingForm, SettingFormItem } from '~components';
+import { Form, Switch } from 'antd';
 
 export const MainSetting: React.FC = () => {
-	const [twoRow, setTwoRow] = useStorage('isTwoRow', false);
+	const [isTwoRow, setIsTwoRow] = useStorage('isTwoRow', false);
 	const [livingList, setLivingList] = useStorage('isLivingList', false);
 	const [isIndexDark, setIsIndexDark] = useStorage('isIndexDark', false);
 	const [isHomeRecRepaint, setIsHomeRecRepaint] = useStorage('isHomeRecRepaint', false);
 	const [isCloseHomeFullScreenPreview, setIsCloseHomeFullScreenPreview] = useStorage('isCloseHomeFullScreenPreview', false);
-	const formConfig: SettingFormItem[] = [
-		{
-			type: FromType.SWITCH,
-			label: '是否开启动态首页双列展示',
-			name: 'isTwoRow',
-			formProps: {
-				checked: twoRow,
-				onClick: (checked) => {
-					setTwoRow(checked);
-				},
-			},
-		},
-		{
-			type: FromType.SWITCH,
-			label: '是否开启【正在直播】列表加强版',
-			name: 'isLivingList',
-			formProps: {
-				checked: livingList,
-				onClick: (checked) => {
-					setLivingList(checked);
-				},
-			},
-		},
-		{
-			type: FromType.SWITCH,
-			label: '是否开启动态首页暗黑模式',
-			name: 'isIndexDark',
-			formProps: {
-				checked: isIndexDark,
-				onClick: (checked) => {
-					setIsIndexDark(checked);
-				},
-			},
-		},
-		{
-			type: FromType.SWITCH,
-			label: '是否开启首页推荐视频布局优化',
-			name: 'isHomeRecRepaint',
-			formProps: {
-				checked: isHomeRecRepaint,
-				onClick: (checked) => {
-					setIsHomeRecRepaint(checked);
-				},
-			},
-		},
-		{
-			type: FromType.SWITCH,
-			label: '是否关闭首页视频全屏预览',
-			name: 'isHomeFullScreenPreview',
-			formProps: {
-				checked: isCloseHomeFullScreenPreview,
-				onClick: (checked) => {
-					setIsCloseHomeFullScreenPreview(checked);
-				},
-			},
-		},
-	];
+	const [form] = Form.useForm();
+
 	return (
-		<SettingForm items={formConfig}></SettingForm>
+		<>
+			<Form
+				form={form}
+				labelCol={{
+					style: {
+						minWidth: '250px',
+					},
+				}}
+				labelWrap
+				labelAlign={'left'}
+				size={'middle'}
+			>
+				<Form.Item
+					label={'是否开启动态首页双列展示'}
+					name={'isTwoRow'}
+				>
+					<Switch
+						checked={isTwoRow}
+						onClick={(checked) => {
+							setIsTwoRow(checked);
+						}}
+					></Switch>
+				</Form.Item>
+				<Form.Item
+					label={'是否开启【正在直播】列表加强版'}
+					name={'isLivingList'}
+				>
+					<Switch
+						checked={livingList}
+						onClick={(checked) => {
+							setLivingList(checked);
+						}}
+					></Switch>
+				</Form.Item>
+				<Form.Item
+					label={'是否开启动态首页暗黑模式'}
+					name={'isIndexDark'}
+				>
+					<Switch
+						checked={isIndexDark}
+						onClick={(checked) => {
+							setIsIndexDark(checked);
+						}}
+					></Switch>
+				</Form.Item>
+				<Form.Item
+					label={'是否开启首页推荐视频布局优化'}
+					name={'isHomeRecRepaint'}
+				>
+					<Switch
+						checked={isHomeRecRepaint}
+						onClick={(checked) => {
+							setIsHomeRecRepaint(checked);
+						}}
+					></Switch>
+				</Form.Item>
+				<Form.Item
+					label={'是否关闭首页视频全屏预览'}
+					name={'isCloseHomeFullScreenPreview'}
+				>
+					<Switch
+						checked={isCloseHomeFullScreenPreview}
+						onClick={(checked) => {
+							setIsCloseHomeFullScreenPreview(checked);
+						}}
+					></Switch>
+				</Form.Item>
+			</Form>
+		</>
 	);
 };
