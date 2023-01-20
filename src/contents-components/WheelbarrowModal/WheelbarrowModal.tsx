735bb7e95da8c623f0872dd7290f31a297924b1e
tuntun <760523867@qq.com>
2023-01-20 17:09:47 +0800

    refactor: 重构发送弹幕方法

--- src/contents-components/WheelbarrowModal/WheelbarrowModal.tsx
+++ src/contents-components/WheelbarrowModal/WheelbarrowModal.tsx
@@ -15,6 +15,35 @@ export const WheelbarrowModal = (props: WheelbarrowModalProps) => {
 	const [isTaskRun, setIsTaskRun] = useState(false);
 	const [msg, setMsg] = useState<string>();
 	const [sendTime, setSendTime] = useState<any>(6);
+	// 发送弹幕v1版本
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const sendDanmuV1 = async () => {
+		try {
+			const data = await API.sendDanmu({
+				msg: msg,
+				roomid: roomid,
+				csrf: Tool.getCookie('bili_jct'),
+			});
+			if (data.code === 10031) {
+				messageApi.error('发送弹幕过快, 请调低发送频率');
+			} else if (data.msg === 'f') {
+				messageApi.error('发送弹幕失败, 请重新编辑内容');
+			}
+		} catch (error) {
+			messageApi.error('发送弹幕失败');
+		}
+	};
+	// 发送弹幕v2版本
+	const sendDanmuV2 = async () => {
+		const textarea: HTMLTextAreaElement = document.querySelector('#chat-control-panel-vm .chat-input');
+		const sendBtn: HTMLButtonElement = document.querySelector('#chat-control-panel-vm .bl-button');
+		if (textarea && sendBtn) {
+			textarea.value = msg;
+			textarea.dispatchEvent(new InputEvent('input'));
+			setTimeout(() => sendBtn.click(), 50);
+		}
+	};
+	// 开始发送
 	const startSend = () => {
 		if (!Tool.isStrNumber(sendTime)) {
 			messageApi.error('时间间隔只能为数字');
@@ -25,26 +54,14 @@ export const WheelbarrowModal = (props: WheelbarrowModalProps) => {
 			return;
 		}
 		const eventId = setInterval(async () => {
-			try {
-				const data = await API.sendDanmu({
-					msg: msg,
-					roomid: roomid,
-					csrf: Tool.getCookie('bili_jct'),
-				});
-				if (data.code === 10031) {
-					messageApi.error('发送弹幕过快, 请调低发送频率');
-				} else if (data.msg === 'f') {
-					messageApi.error('发送弹幕失败, 请重新编辑内容');
-				}
-			} catch (error) {
-				messageApi.error('发送弹幕失败');
-			}
+			sendDanmuV2();
 		}, parseInt(sendTime, 10) * 1000);
 		id.current = eventId;
 		setIsTaskRun(true);
 		log('开始独轮车');
 		messageApi.success('开始独轮车');
 	};
+	// 结束发送
 	const stopSend = () => {
 		clearInterval(id.current);
 		setIsTaskRun(false);
